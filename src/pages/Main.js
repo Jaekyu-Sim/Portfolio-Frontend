fedb38e1d1540c9f00d17a00bde2caf6c8f60234
jaekyu-sim <worb1605@gmail.com>
2022-06-21 00:59:17 +0900

    Main Page Routing and Layout

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,22 @@
 import React from "react";
+import {Layout} from "antd" ;
+import '../App.css';
+
+const {Header, Footer, Sider, Content} = Layout;
 
 const Main = () => {
     return (
         <div>
-            Main Page
+            <Layout className="layout">
+                <Sider>
+                    Sider
+                </Sider> 
+                <Layout>
+                    <Header>Header</Header>
+                    <Content>Content</Content>
+                    <Footer>Footer</Footer>
+                </Layout>   
+            </Layout>
         </div>
     )
 }
