874d925954698855ab7bf84a016c0bfdfe1033f4
jaekyu-sim <worb1605@naver.com>
2022-06-30 22:38:59 +0900

    docker file create

---
+++ build/precache-manifest.f3fa0844f82c650f3b5bbebbd0df47b4.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "14bd344f317fdf8fd059",
+    "url": "/static/css/main.e6e245dc.chunk.css"
+  },
+  {
+    "revision": "14bd344f317fdf8fd059",
+    "url": "/static/js/main.953dbfc0.chunk.js"
+  },
+  {
+    "revision": "ba28ee30919e37a10714",
+    "url": "/static/js/runtime~main.c47268d3.js"
+  },
+  {
+    "revision": "c51305a7f3d481af446c",
+    "url": "/static/css/2.a82a8aa2.chunk.css"
+  },
+  {
+    "revision": "c51305a7f3d481af446c",
+    "url": "/static/js/2.20d925fc.chunk.js"
+  },
+  {
+    "revision": "d3e964dbb834533e9b1e",
+    "url": "/static/js/3.b54b0b01.chunk.js"
+  },
+  {
+    "revision": "dced2b4b2f0924d8e8cfdad41518d02c",
+    "url": "/static/media/PoseEstimationOutput.dced2b4b.PNG"
+  },
+  {
+    "revision": "32f1e38a5af8abdb7e9755f1d52e8e32",
+    "url": "/static/media/HandKeypointDetectionOutput.32f1e38a.PNG"
+  },
+  {
+    "revision": "46af2f0394c73a55ddab325dd4e1cef0",
+    "url": "/static/media/SKCC.46af2f03.png"
+  },
+  {
+    "revision": "9d2325e14da1ba430005a5cfe4cf2642",
+    "url": "/static/media/블로그1.9d2325e1.png"
+  },
+  {
+    "revision": "f68b194b2c87fdd5019550460a2571a7",
+    "url": "/static/media/개발자1.f68b194b.png"
+  },
+  {
+    "revision": "1f516e71fb55b132bca758e0da940e0c",
+    "url": "/static/media/개발자2.1f516e71.png"
+  },
+  {
+    "revision": "e58c83d35a0d11c723458aa29d4b6939",
+    "url": "/static/media/개발자3.e58c83d3.png"
+  },
+  {
+    "revision": "52db424f43441bca10a7ec2b682b3e56",
+    "url": "/static/media/개발자4.52db424f.png"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/static/media/githubIcon.1bb02710.svg"
+  },
+  {
+    "revision": "68597585ab61b1274c3613ae3eb1c3a9",
+    "url": "/static/media/Title.68597585.png"
+  },
+  {
+    "revision": "0e714eadcc6f62ad88960706ce0b21ee",
+    "url": "/static/media/Title2.0e714ead.PNG"
+  },
+  {
+    "revision": "9c181982dd8ab1510f634bc250d0b390",
+    "url": "/static/media/내사진2.9c181982.jpg"
+  },
+  {
+    "revision": "96936550fd1fb0d2bb5012074295666b",
+    "url": "/static/media/우리사진.96936550.jpg"
+  },
+  {
+    "revision": "ae3019ec7e30b1f7e7353108ffc34cff",
+    "url": "/static/media/졸업작품.ae3019ec.PNG"
+  },
+  {
+    "revision": "610f29000ad95c831f100e16a52e3153",
+    "url": "/static/media/001.610f2900.jpg"
+  },
+  {
+    "revision": "43067391cb6bfeca610b58919d58752f",
+    "url": "/static/media/002.43067391.jpg"
+  },
+  {
+    "revision": "44c0717fe4f250dde4c2dc39d8768890",
+    "url": "/static/media/003.44c0717f.jpg"
+  },
+  {
+    "revision": "8eab8e229a6f3519a079060706894b1d",
+    "url": "/static/media/004.8eab8e22.jpg"
+  },
+  {
+    "revision": "5de65e29474ddc8b22cc39aa245c3ac2",
+    "url": "/static/media/005.5de65e29.jpg"
+  },
+  {
+    "revision": "2c5efdb4fd87642d82f448b42b263b44",
+    "url": "/static/media/githubResult1.2c5efdb4.png"
+  },
+  {
+    "revision": "6f6ce5fbc2c8965bed582659639e9865",
+    "url": "/index.html"
+  }
+];
